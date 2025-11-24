a66dc2da58843d4ee271bd6c65b0b6dab0523d66
lauti <ltsoria@fi.uba.ar>
2025-11-24 18:13:44 -0300

    verificacion de admin en gestionar usuarios

--- front/app.py
+++ front/app.py
@@ -109,24 +109,7 @@ def generarqr():
 def perfil():
     data = get_jwt()
     usuario_id = get_jwt_identity()
-    es_proveedor = False
 
-    if data and data.get("isProveedor"):
-        es_proveedor = True
-        response = requests.get(f"{BACKEND_URL}/proveedores/{usuario_id}")
-        
-        if response.status_code != 200:
-            return "Proveedor no encontrado", 404
-        
-        datos_proveedor = response.json()
-        
-        return render_template(
-            "editar_perfil.html",          
-            es_proveedor=es_proveedor,
-            usuario=datos_proveedor,
-            data=data
-        )
-    
     response = requests.get(f"{BACKEND_URL}/usuarios/{usuario_id}")
 
     if response.status_code != 200:
@@ -146,8 +129,6 @@ def perfil():
     return render_template(
         "editar_perfil.html",
         usuario=datos_user,
-        es_proveedor=es_proveedor,
-        data=data
     )
 
 
@@ -359,8 +340,29 @@ def eliminar_usuario(id):
     
     
     return redirect(url_for('listar_usuarios'))
+
+
 @app.route('/usuarios')
+@jwt_required(locations=['cookies'])
 def ver_usuarios():
+    data = get_jwt()
+    usuario_id = get_jwt_identity()
+    
+    user_data = data if data else None
+
+    if user_data is None:
+        return redirect(url_for('auth'))
+    
+    response_user = requests.get(f"{BACKEND_URL}/usuarios/{usuario_id}")
+
+    if response_user.status_code != 200:
+        return "Usuario no encontrado", 404
+
+    datos_user = response_user.json()
+
+    if datos_user.get("rol") != "admin":
+        return "permiso denegado", 404
+    
     response = requests.get(f'{BACKEND_URL}/usuarios/todos')
     
     if response.status_code != 200:
@@ -369,5 +371,8 @@ def ver_usuarios():
     usuarios = response.json()
 
     return render_template('usuarios.html',usuarios=usuarios)
+
 if __name__ == '__main__':
     app.run("localhost", port=1230, debug=True)
+
+
