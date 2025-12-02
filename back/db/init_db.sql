3641cdc5875742ebe37c8d861beca779d9b2c145
lauti <ltsoria@fi.uba.ar>
2025-12-02 15:10:20 -0300

    el campo imagen de servicios ahora es un numero random entre 1 y 10 que se asigna al registrarlo, en el template muestra la imagen correspondiente a ese numero

--- back/db/init_db.sql
+++ back/db/init_db.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS servicios (
   categoria_id UUID NOT NULL,
   nombre VARCHAR(255) NOT NULL,
   descripcion TEXT,
-  imagen VARCHAR(255),
+  imagen INT CHECK (imagen BETWEEN 1 AND 10),
   precio DECIMAL(10,2) NOT NULL,
   hora_inicio INT CHECK (hora_inicio BETWEEN 1 AND 24),
   hora_fin INT CHECK (hora_fin BETWEEN 1 AND 24),
